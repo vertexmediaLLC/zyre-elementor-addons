09988327f61a5207b407fae8757c4a8fe37cd861
trainershujon <trainershujon@gmail.com>
2026-02-07 16:08:44 +0600

    heading removed from subscription modal

--- templates/admin/subscription-modal.php
+++ templates/admin/subscription-modal.php
@@ -9,7 +9,6 @@ defined( 'ABSPATH' ) || die();
 <div id="zyre-subscription-modal" class="zyre-modal zyre-subscription-modal video-modal-shown">
 	<div class="zyre-modal-content">
 		<span class="zyre-modal-close">&times;</span>
-		<h3 style="margin-bottom: 15px;"><?php _e( 'Get Zyre Addons <span>Pro</span> Notification', 'zyre-elementor-addons' ); // phpcs:ignore WordPress.Security.EscapeOutput ?></h3>
 		<iframe style="min-height: 220px;width:100%;" src="<?php echo esc_url( 'https://templates.zyreaddons.com/zyreaddons-subscription-form/' ); ?>" frameborder="0"></iframe>
 	</div>
 </div>
