7eaf83da29f7c4d0698c3b93dd5ebbfe6978a36e
vertexmediaLLC <hello@vertexmedia.tech>
2026-08-20 17:16:50 +0600

    Updated Button, Call to Action, and Image List widgets

--- zyre-elementor-addons.php
+++ zyre-elementor-addons.php
@@ -5,7 +5,7 @@
  * Plugin Name: Zyre Elementor Addons
  * Description: <a href="https://zyreaddons.com/">Zyre Elementor Addons</a> is a powerful Lightweight Elementor addons with Theme Builder, Header/Footer and Mega Menu Builder, 60+ widgets including Off-Canvas, and 54+ pre-built templates.
  * Plugin URI: https://zyreaddons.com/
- * Version: 2.1.1
+ * Version: 2.1.2
  * Author: VertexMediaLLC
  * Author URI: https://vertexmedia.tech/
  * Text Domain: zyre-elementor-addons
@@ -21,7 +21,7 @@
  *
  * [WP]
  * Requires at least: 5.0
- * Tested up to: 7.0
+ * Tested up to: 7.1
  *
  * @package ZyreAddons
  */
@@ -48,7 +48,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'ZYRELADDONS_VERSION', '2.1.1' );
+define( 'ZYRELADDONS_VERSION', '2.1.2' );
 define( 'ZYRELADDONS__FILE__', __FILE__ );
 define( 'ZYRELADDONS_DIR_PATH', plugin_dir_path( ZYRELADDONS__FILE__ ) );
 define( 'ZYRELADDONS_DIR_URL', plugin_dir_url( ZYRELADDONS__FILE__ ) );
